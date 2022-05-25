aeb68b5112b2e9a3298515b502d505b440bac6e2
Shravya Nandyala <shravya.nandyala@gmail.com>
2022-05-25 11:17:53 -0700

    testing mutex locks

--- bigger-proj/main.go
+++ bigger-proj/main.go
@@ -1,5 +1,32 @@
+// modify the contents of an array with different go routines
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var m sync.Mutex
+
+func main() {
+	var wg sync.WaitGroup
+	arr := []int{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(val int) {
+			defer wg.Done()
+			addVal(&arr, val)
+		}(i)
+	}
+	wg.Wait()
+	for _, i := range arr {
+		fmt.Println(i)
+	}
+}
+
+func addVal(arr *[]int, val int) {
+	m.Lock()
+	*arr = append(*arr, val)
+	m.Unlock()
 }

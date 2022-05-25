f847e0b46bee7e496d4703b5c5a55dbb1c088750
Shravya Nandyala <shravya.nandyala@gmail.com>
2022-05-25 11:02:09 -0700

    trying out mutex locks in go

--- http-get/main.go
+++ http-get/main.go
@@ -3,35 +3,45 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
 	sites := []string{
 		"http://cisco.com",
 		"http://golang.org",
+		"http://nyt.com",
+		"http://wsj.com",
 	}
 
-	c := make(chan string)
-
+	// using locks instead of channels
 	for _, site := range sites {
-		go checkStatus(site, c)
-	}
-	for site := range c {
+		wg.Add(1)
 		go func(s string) {
-			time.Sleep(time.Second)
-			checkStatus(s, c)
+			defer wg.Done()
+			checkStatus(s)
 		}(site)
 	}
+	wg.Wait()
+	fmt.Println("Done with all go routines.")
+
+	// channels stuff
+	// c := make(chan string)
+	// for site := range c {
+	//	go func(s string) {
+	//		time.Sleep(time.Second)
+	//		checkStatus(s, c)
+	//	}(site)
+	// }
 }
 
-func checkStatus(site string, c chan string) {
+func checkStatus(site string) {
 	_, err := http.Get(site)
 	if err != nil {
 		fmt.Println(site, "GET request has failed.")
-		c <- site
 		return
 	}
 	fmt.Println(site, "GET request has succeeded.")
-	c <- site
 }

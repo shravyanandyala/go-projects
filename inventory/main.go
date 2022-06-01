0ba2e52a796606464062cf39ad148be5b91d2810
Shravya Nandyala <shravya.nandyala@gmail.com>
2022-06-01 13:24:56 -0700

    test

---
+++ inventory/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No input file provided.")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", os.Args[1])
+		panic(err)
+	}
+
+}
